d03677f63049a698a13f24812fe58615314cf74e
AndrexDynasty <andresw3113@gmail.com>
2025-09-24 17:13:07 +0000

    mas

---
+++ Clase 5/EjecutarColaU2.java
@@ -0,0 +1,9 @@
+public class EjecutarColaU2 {
+    public static void main(String[] args) {
+       
+        contarvotos cola = new contarvotos();
+       
+        int resultado = cola.contarvotos(132, 132);
+        System.out.println("Resultado de votos: " + resultado); // Debería imprimir 2
+    }
+}
---
+++ Clase 5/contarvotos.java
@@ -0,0 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class contarvotos {
+    public int contarvotos(int upvotes,int downvotes) {
+// 1) Crear una cola para contar los votos
+        Queue<Integer> VotosPos = new ArrayDeque<>();
+        Queue<Integer> VotosNeg = new ArrayDeque<>();
+        for(int i =1;i<=upvotes;i++){
+            VotosPos.add(1);
+        }
+         for(int i =1;i<=downvotes;i++){
+            VotosNeg.add(1);
+        }
+    return VotosPos.size()-VotosNeg.size();
+    }
+    
+        
+    
+}
